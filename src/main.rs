690d58d6195a163562e71c7bf6bd3807d3a9d8f9
Philipp Brendel <philipp.brendel@ianeo.de>
2024-08-29 15:00:58 +0200

    fix output behavior

--- src/main.rs
+++ src/main.rs
@@ -52,12 +52,18 @@ fn main() {
 
 fn process_input(config: Config) -> Result<u128, Error> {
     let mut match_count = 0;
-    // If file arguments have not been provided, read from stdin
+    // Create a regex pattern that is case insensitive if the insensitive flag is set
+    let pattern = if config.insensitive {
+        Regex::new(&format!("(?i){}", config.pattern)).unwrap()
+    } else {
+        config.pattern.clone()
+    };
+
     if config.files.is_empty() {
         // No files provided, read from stdin
         let stdin = io::stdin();
         let reader = stdin.lock();
-        match_count = process_lines(reader, &config, None)?;
+        match_count = process_lines(reader, &config, &pattern, None)?;
         if config.count_only && match_count > 0 && !config.filename_only {
             println!("{}", match_count);
         }
@@ -66,7 +72,7 @@ fn process_input(config: Config) -> Result<u128, Error> {
         for filename in &config.files {
             if let Ok(file) = File::open(&filename) {
                 let reader = BufReader::new(file);
-                match_count += process_lines(reader, &config, Some(&filename))?;
+                match_count += process_lines(reader, &config, &pattern, Some(&filename))?;
                 if match_count > 0 && config.count_only && !config.filename_only {
                     if config.files.len() > 1 {
                         println!("{}:{}", filename, match_count);
@@ -84,14 +90,10 @@ fn process_input(config: Config) -> Result<u128, Error> {
 }
 
 // Function to process lines from a reader
-fn process_lines<R: BufRead>(reader: R, config: &Config, filename: Option<&str>) -> Result<u128, Error> {
+fn process_lines<R: BufRead>(reader: R, config: &Config, pattern: &Regex, filename: Option<&str>) -> Result<u128, Error> {
     let mut line_number: u128 = 0;
     let mut match_count: u128 = 0;
-    let pattern = if config.insensitive {
-        Regex::new(&format!("(?i){}", config.pattern)).unwrap()
-    } else {
-        config.pattern.clone()
-    };
+    
     for line in reader.lines() {
         line_number += 1;
         match line {
@@ -120,10 +122,18 @@ fn process_lines<R: BufRead>(reader: R, config: &Config, filename: Option<&str>)
                     // Print the file name unless the count only flag is set
                     if !config.count_only {
                         if let Some(filename) = filename {
-                            if config.line_number {
-                                println!("{}:{}:{}", filename, line_number, content);
+                            if config.files.len() > 1 {
+                                if config.line_number {
+                                    println!("{}:{}:{}", filename, line_number, content);
+                                } else {
+                                    println!("{}:{}", filename, content);
+                                }
                             } else {
-                                println!("{}:{}", filename, content);
+                                if config.line_number {
+                                    println!("{}:{}", line_number, content);
+                                } else {
+                                    println!("{}", content);
+                                }
                             }
                         } else {
                             if config.line_number {
